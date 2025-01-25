28ae9f907f29957ee103f92d2ad6a047d4bc859f
justkashish <meghanikashish1@gmail.com>
2025-01-25 06:49:54 +0530

    added mobile no in schema

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken');
 
 const signup = async(req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const { name, email, password, mobile } = req.body;
         const user = await UserModel.findOne({ email });
         if (user) {
             return res.status(409)
                 .json({ message: "User already exist, you can login", success: false });
         }
-        const userModel = new UserModel({ name, email, password });
+        const userModel = new UserModel({ name, email, password , mobile });
         userModel.password = await bcrypt.hash(password, 10);
 
         await userModel.save();
